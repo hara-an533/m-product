622ff2e3ea51c0b6b0822bd6606ce23e41bfbcbc
lengn <2497963360@qq.com>
2020-05-27 20:31:05 +0800

    "0527"

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -9,11 +9,18 @@
         <el-calendar>
           <template slot="dateCell" slot-scope="{date, data}">
             <p
-              :class="data.isSelected ? 'is-selected' : ''"
+              :class="data.isSelected ? 'is-show' : ''"
+              click="tab"
+              ref="calendar"
             >{{ data.day.split('-').slice(1).join('-') }} {{ data.isSelected ? '✔️' : ''}}</p>
           </template>
         </el-calendar>
       </el-main>
+      <div class="cover" :class="[isshow? 'block':'none']">
+        <div class="form">
+          <h1>{{time}}</h1>
+        </div>
+      </div>
     </el-container>
   </div>
 </template>
@@ -23,13 +30,31 @@ export default {
   name: "Detail",
   data() {
     return {
-      value: new Date()
+      value: new Date(),
+      isshow: false,
+      time:''
     };
   },
   methods: {
     logout() {
       window.sessionStorage.clear();
       this.$router.push("/login");
+    },
+    tab() {
+      alert("123");
+    }
+  },
+  mounted() {
+    let dt = document.querySelectorAll(".el-calendar-day");
+    for (let i = 0; i < dt.length; i++) {
+      dt[i].onclick = function() {
+        // event.preventDefault();
+        this.isshow = true;
+        console.log(this.isshow);
+        this.time=dt[i].innerText;
+        console.log(this.time);
+      };
+      this.isshow=false;
     }
   }
 };
@@ -41,19 +66,41 @@ export default {
   height: 100%;
 }
 .el-header {
-  background-color: #d5d5d5;
+  background-color: #e0e0c9;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 .el-main {
-  background-color: #f7f7f7;
-}
-.el-main {
   padding: 400px;
   padding-top: 100px;
+  background-color: #f7f7f7;
 }
 .is-selected {
   color: #1989fa;
+}
+.block {
+  display: block;
+}
+.none{
+  display: none;
+}
+.cover {
+  position: fixed;
+  top: 60px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(1, 2, 1, 0.1);
+}
+.form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  height: 400px;
+  background-color: #fff;
+  z-index: 100;
 }
 </style>
